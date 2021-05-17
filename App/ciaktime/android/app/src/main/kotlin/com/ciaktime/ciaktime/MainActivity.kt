792a3e1bf295bdcd7a38166afe8356fb6e69ddd6
Mauro <40062845+mauroficorella@users.noreply.github.com>
2021-05-17 22:31:17 +0200

    app

---
+++ App/ciaktime/android/app/src/main/kotlin/com/ciaktime/ciaktime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ciaktime.ciaktime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ App/ciaktime/lib/Login/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:ciaktime/Login/components/body.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
+
+
+
+    
