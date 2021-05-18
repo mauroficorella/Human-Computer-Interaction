a3c8826269e46698caef21adb7a882aea5b7bbc7
Mauro <40062845+mauroficorella@users.noreply.github.com>
2021-05-18 19:16:43 +0200

    app funzionante

---
+++ App/ciak_time/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:ciak_time/Login/welcome_screen.dart';
+import 'package:ciak_time/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+
