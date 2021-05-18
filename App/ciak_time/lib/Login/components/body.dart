c305cb27eae667273f95925bfaad4cf75dc06b61
Mauro <40062845+mauroficorella@users.noreply.github.com>
2021-05-18 20:15:13 +0200

    app working

---
+++ App/ciak_time/lib/Login/components/body.dart
@@ -0,0 +1,69 @@
+import 'package:ciak_time/Login/components/background.dart';
+import 'package:ciak_time/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: Container(
+        margin: EdgeInsets.fromLTRB(0.0, 55.0, 0.0, 0.0),
+        child: SingleChildScrollView(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "CiakTime",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60.0,
+                    fontFamily: 'Pattaya',
+                    color: kPrimaryColor),
+              ),
+              SvgPicture.asset("assets/icons/popcorn.svg",
+                  width: size.width * 0.45),
+              Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 36),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(29),
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                    color: kPrimaryColor,
+                    onPressed: () {},
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
